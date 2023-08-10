e9e977e8bed413d40457a120cdd008ea8f2c6d77
Pankaj Goyal <pankajg@ip-192-168-1-8.ap-southeast-1.compute.internal>
2023-08-10 20:10:36 +0530

    debit card & txns test cases

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\DebitCard;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
@@ -12,48 +13,83 @@ class DebitCardTransactionControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected User $user;
-    protected DebitCard $debitCard;
+    protected User $user1;
+    protected User $user2;
+    protected DebitCard $debitCard1;
+    protected DebitCard $debitCard2;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->user = User::factory()->create();
-        $this->debitCard = DebitCard::factory()->create([
-            'user_id' => $this->user->id
+
+        //Creating Test data
+        $this->user1 = User::factory()->create();
+        Passport::actingAs($this->user1);
+        $this->debitCard1 = DebitCard::factory()->create([
+            'user_id' => $this->user1->id
         ]);
-        Passport::actingAs($this->user);
+        $this->debitCard1Details = json_decode($this->debitCard1,true);
+
+        $this->user2 = User::factory()->create();
+        Passport::actingAs($this->user2);
+        $this->debitCard2 = DebitCard::factory()->create([
+            'user_id' => $this->user2->id
+        ]);
+        $this->debitCard2Details = json_decode($this->debitCard2,true);
     }
 
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
-        // get /debit-card-transactions
+        $this->json('GET','/api/debit-card-transactions?debit_card_id='.$this->debitCard2Details['id'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                '*'=>[
+                    'amount',
+                    'number',
+                    'debit_card_id',
+                    'id'
+                ]
+            ]);
     }
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
     {
-        // get /debit-card-transactions
+        $this->json('GET','/api/debit-card-transactions?debit_card_id='.$this->debitCard1Details['id'])
+            ->assertStatus(Response::HTTP_FORBIDDEN);
     }
 
     public function testCustomerCanCreateADebitCardTransaction()
     {
-        // post /debit-card-transactions
+        $response = $this->json('POST','/api/debit-card-transactions',['debit_card_id'=>$this->debitCard2Details['id'],'amount'=>'500','currency_code'=>'SGD'])->assertStatus(Response::HTTP_CREATED);;
+        $txnDetails = json_decode($response->getContent(),true);
+        $this->assertDatabaseHas('debit_card_transactions', [
+            'amount' => $txnDetails['amount'],
+            'currency_code' => $txnDetails['currency_code'],
+            'debit_card_id' => $this->debitCard2Details['id']
+        ]);
     }
 
     public function testCustomerCannotCreateADebitCardTransactionToOtherCustomerDebitCard()
     {
-        // post /debit-card-transactions
+        $this->json('POST','/api/debit-card-transactions',['debit_card_id'=>$this->debitCard1Details['id'],'amount'=>'500','currency_code'=>'SGD'])
+            ->assertStatus(Response::HTTP_FORBIDDEN);
     }
 
     public function testCustomerCanSeeADebitCardTransaction()
     {
-        // get /debit-card-transactions/{debitCardTransaction}
+        $this->json('GET','/api/debit-card-transactions?debit_card_id='.$this->debitCard2Details['id'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                '*'=>[
+                    'amount',
+                    'currency_code',
+                ]
+            ]);
     }
 
     public function testCustomerCannotSeeADebitCardTransactionAttachedToOtherCustomerDebitCard()
     {
-        // get /debit-card-transactions/{debitCardTransaction}
+        $this->json('GET','/api/debit-card-transactions?debit_card_id='.$this->debitCard1Details['id'])
+            ->assertStatus(Response::HTTP_FORBIDDEN);
     }
-
-    // Extra bonus for extra tests :)
 }
